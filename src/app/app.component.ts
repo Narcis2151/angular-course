655bd95c82a9103c8a25c2d87f2ac01b12fae092
narcis.fanica <narcis.fanica@getik.net>
2023-12-17 15:19:18 +0200

    Update environment and tsconfig files, fix styling issues, and add CourseCard component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
-import {COURSES} from '../db-data';
+import { Component } from "@angular/core";
+import { COURSES } from "../db-data";
+import { Course } from "./model/course";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-
-
-
+  onCourseViewed(course: Course) {
+    console.log("App Component - Course Card Clicked: ", course);
+  }
+  coreCourse = COURSES[0];
+  rxjsCourse = COURSES[1];
+  ngrxCourse = COURSES[2];
 }
---
+++ src/app/course-card/course-card.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, Output, EventEmitter } from "@angular/core";
+import { Course } from "../model/course";
+
+@Component({
+  selector: "course-card",
+  templateUrl: "./course-card.component.html",
+  styleUrl: "./course-card.component.css",
+})
+export class CourseCardComponent {
+  @Input({ required: true })
+  course: Course;
+
+  @Output()
+  courseSelected = new EventEmitter<Course>();
+
+  onCourseViewed() {
+    console.log("Course-Card Component clicked...ready to print info");
+    this.courseSelected.emit(this.course);
+  }
+}
